4dcac0d7ef96820e74689e7eaffaf15b22427d58
Callkehinde <callkehindealways2@gmail.com>
2023-01-31 18:00:27 -0800

    0x06-python-classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,10 +5,10 @@
 
 class Square:
     """Class defined for square generation.
-    
+
     Args:
     size (int): length of one side of square
-    
+
     Attributes:
     __size (int): length of one side of square
 
