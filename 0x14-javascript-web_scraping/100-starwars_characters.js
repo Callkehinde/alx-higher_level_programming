96b8fb8fae5eef7864eac0cc31314af467284db2
Callkehinde <Callkehindealways2@gmail.com>
2023-04-27 02:30:24 -0700

    web_scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,35 +1,14 @@
 #!/usr/bin/node
-// Script that prints all characters of a Star Wars movie:
-
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (err, response, body)
-{
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-    if (err)
-    {
-        console.log(err);
-    }
-    else
-    {
-        let characters = JSON.parse(body).characters;
-        for (let i = 0; i < characters.length; i++)
-        {
-            request(characters[i], function (err, response, body)
-            {
-                if (err)
-                {
-                    console.log(err);
-                }
-                else
-                {
-                    console.log(JSON.parse(body).name);
-                }
-            }
-            );
-        }
-    }
+request(starWarsUri, function (_err, _res, body) {
+  const characters = JSON.parse(body).characters;
 
-}
-);
+  for (let i = 0; i < characters.length; ++i) {
+    request(characters[i], function (_cErr, _cRes, cBody) {
+      console.log(JSON.parse(cBody).name);
+    });
+  }
+});
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,35 +1,25 @@
 #!/usr/bin/node
-// Script that computes the number of tasks completed by user id.
 
 const request = require('request');
-const url = process.argv[2];
 
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
 
-request(url, function (err, response, body)
-{
-    if (err)
-    {
-        console.log(err);
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
     }
-    else
-    {
-        let completedTasks = {};
-        let tasks = JSON.parse(body);
-        for (let i = 0; i < tasks.length; i++)
-        {
-            if (tasks[i].completed === true)
-            {
-                if (completedTasks[tasks[i].userId] === undefined)
-                {
-                    completedTasks[tasks[i].userId] = 1;
-                }
-                else
-                {
-                    completedTasks[tasks[i].userId] += 1;
-                }
-            }
-        }
-        console.log(completedTasks);
-    }
-}
-);
+
+    console.log(completedTasksByUsers);
+  }
+});
