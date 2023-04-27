e5e20f4ebc058a31b8875100d6b3d78b6fa0ebcc
Callkehinde <Callkehindealways2@gmail.com>
2023-04-26 23:06:01 -0700

    javascipt-web

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Prints the title of a Star Wars movie where the episode number matched a given integer.
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (err, response, body)
+{
+    if (err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log(JSON.parse(body).title);
+    }
+}
+);
